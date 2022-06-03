632abc7e4373db7f9e2f68685a3fc51ef3c70528
Your Name <you@example.com>
2022-06-04 00:38:24 +0300

    finished ModHash

--- ModHash.java
+++ ModHash.java
@@ -1,13 +1,20 @@
+import java.util.Random;
 
 public class ModHash {
-	
+	private int a, b, m;
+	private long p;
+	public ModHash(int a, int b, int m, long p){
+		this.a = a;
+		this.b = b;
+		this.m = m;
+		this.p = p;
+	}
 	public static ModHash GetFunc(int m, long p){
-		// TODO implement random choice of mod function from family
-		return null;
+		Random rnd = new Random();
+		return new ModHash(rnd.nextInt(), rnd.nextInt(), m, p);
 	}
 	
 	public int Hash(long key) {
-		// TODO implement hash function
-		return 0;
+		return ((a*(int)key + b)%(int)p)%m;
 	}
 }
