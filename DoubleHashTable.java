35042a6856afa67e605b0ffadd0e12ef754f74f2
galimaman <100682182+galimaman@users.noreply.github.com>
2022-06-16 00:12:43 +0300

    Update DoubleHashTable.java

--- DoubleHashTable.java
+++ DoubleHashTable.java
@@ -1,16 +1,25 @@
 import java.util.Random;
 
 public class DoubleHashTable extends OAHashTable {
-	
+	private ModHash firstHashFunc;
+	private ModHash secondHashFunc;
+
 	public DoubleHashTable(int m, long p) {
 		super(m);
+		this.firstHashFunc = ModHash.GetFunc(m,p);
+		this.secondHashFunc = ModHash.GetFunc(m,p);
 		// TODO Complete hash table constructor.
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
 		// TODO implement hash function
-		return 0;
+		int hash = this.firstHashFunc.Hash(x) + this.secondHashFunc.Hash(x) * i;
+		hash = hash % this.getTableLenght();
+		if (hash < 0){
+			hash += this.getTableLenght();
+		}
+		return hash;
 	}
 	
 }
