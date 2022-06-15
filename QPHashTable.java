d50066a016c45eff5dfa39693121d1bed4ddec3b
galimaman <100682182+galimaman@users.noreply.github.com>
2022-06-16 00:14:38 +0300

    Update QPHashTable.java

--- QPHashTable.java
+++ QPHashTable.java
@@ -1,15 +1,23 @@
 import java.util.Random;
+import java.lang.Math;
 
 public class QPHashTable extends OAHashTable {
+	private ModHash hashFunc;
 
 	public QPHashTable(int m, long p) {
 		super(m);
+		this.hashFunc = ModHash.GetFunc(m,p);
 		// TODO Complete hash table constructor.
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
 		// TODO implement hash function
-		return 0;
+		int hash = this.hashFunc.Hash(x) + (int) Math.pow(i,2);
+		hash = hash % this.getTableLenght();
+		if (hash < 0){
+			hash += this.getTableLenght();
+		}
+		return hash;
 	}
 }
