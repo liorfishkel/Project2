43235b246a47999a62bbd57392a7bcf9b5c07c3b
galimaman <100682182+galimaman@users.noreply.github.com>
2022-06-16 00:12:18 +0300

    Update AQPHashTable.java

--- AQPHashTable.java
+++ AQPHashTable.java
@@ -1,15 +1,22 @@
 import java.util.Random;
 
 public class AQPHashTable extends OAHashTable {
+	private ModHash hashFunc;
 
 	public AQPHashTable(int m, long p) {
 		super(m);
+		this.hashFunc = ModHash.GetFunc(m,p);
 		// TODO Complete hash table constructor.
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
 		// TODO implement hash function
-		return 0;
+		int hash = this.hashFunc.Hash(x) + (int) Math.pow(-1,i) * (int) Math.pow(i,2);
+		hash = hash % this.getTableLenght();
+		if (hash < 0){
+			hash += this.getTableLenght();
+		}
+		return hash;
 	}
 }
