6e114852a9f894650cafacf99f08234638301388
galimaman <100682182+galimaman@users.noreply.github.com>
2022-06-16 00:13:11 +0300

    Update LPHashTable.java

--- LPHashTable.java
+++ LPHashTable.java
@@ -1,16 +1,23 @@
 import java.util.Random;
 
 public class LPHashTable extends OAHashTable {
-	
+	private ModHash hashFunc;
+
 	public LPHashTable(int m, long p) {
 		super(m);
+		this.hashFunc = ModHash.GetFunc(m,p);
 		// TODO Complete hash table constructor.
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
 		// TODO implement hash function
-		return 0;
+		int hash = this.hashFunc.Hash(x) + i;
+		hash = hash % this.getTableLenght();
+		if (hash < 0){
+			hash += this.getTableLenght();
+		}
+		return hash;
 	}
 	
 }
